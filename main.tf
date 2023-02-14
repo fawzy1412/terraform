783200a9dff4211a7a98a89dabb65e26d6a292b1
fawzy1412 <fawzy141219@gmail.com>
2023-02-14 11:44:16 +0200

    edit provider

--- main.tf
+++ main.tf
@@ -1,14 +1,29 @@
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# resource "aws_s3_bucket" "bucket1" {
+#   bucket = "terraform-54321"
+#    acl    = "private"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
+
 provider "aws" {
-  region = "us-east-1"
+  profile = "myaws"
+  region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "bucket1" {
-  bucket = "terraform-54321"
-   acl    = "private"
+
+resource "aws_s3_bucket" "b" {
+  bucket = "myawsbucket12345554555525"
+  acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
-
